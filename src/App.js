83594d880bf9a1c4b971316c751579fac329c409
Александр Раковчен <italianhorse20@gmail.com>
2023-05-18 23:54:13 +0300

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import HomePage from './Components/HomePage.js';
+import DriftPage from './Components/DriftPage.js';
+import TimeAttackPage from './Components/TimeAttackPage.js';
+import ForzaPage from './Components/ForzaPage.js';
+ 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  function Menu() {
+    return <div className='menu'>
+        <NavLink to='/' className='menu__item'>Главная</NavLink>
+        <NavLink to='/drift' className='menu__item'>Дрифт-такси</NavLink>
+        <NavLink to='/timeattack' className='menu__item'>Time Attack</NavLink>
+        <NavLink to='/forza' className='menu__item'>Forza Karting</NavLink>
     </div>
+  };
+
+  return (
+      <div>
+        <Menu />
+        <Routes className="page">
+          <Route path="/" exact element={ <HomePage /> } />
+          <Route path="/drift" element={ <DriftPage /> } />
+          <Route path="/timeattack" element={ <TimeAttackPage /> } />
+          <Route path="/forza" element={ <ForzaPage /> } />
+        </Routes>
+      </div>
   );
 }
 
